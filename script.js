f05a94959b140a07dc785c43c765478f6dcc009d
vanshu-himself <124697864+vanshu-himself@users.noreply.github.com>
2023-05-20 22:30:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function insert_Row() {
     //Write your code here
-  
+  var tableRow = document.getElementById("sampleTable");
+var row = tableRow.insertRow(0);
+var cell1 = row.insertCell(0);
+var cell2 = row.insertCell(1);
+
+ cell1.innerHTML = "New Cell1";
+ cell2.innerHTML = "New Cell2";
+
   
 }
